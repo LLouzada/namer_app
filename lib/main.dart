7e22edaef282012babfd8a1c6f5f78897701b1a2
LLouzada <loumad.soft@gmail.com>
2019-09-27 10:19:34 -0300

    implements favoritos

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         ),
         home: MyHomePage(),
       ),
@@ -47,6 +47,21 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  /// A new property to MyAppState called favorites. This property is initialized with an empty list: []
+  /// Then, specified that the list can only ever contain word pairs: [<WordPair>[]], using generics.
+  /// This helps make your app more robust — Dart refuses to even run your app if you try to add anything other than WordPair to it.
+  /// In turn, you can use the favorites list knowing that there can never be any unwanted objects (like null) hiding in there.
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 /// The [MyHomePage] widget is the starting point of the app.
@@ -57,6 +72,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // [MyHomePage] tracks changes to the app's current state using the watch method.
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     // Every build method must return a widget or (more typically) a nested tree of widgets.
     // In this case, the top-level widget is [Scaffold]. You aren't going to work with Scaffold in this codelab,
@@ -65,23 +81,81 @@ class MyHomePage extends StatelessWidget {
       // [Column] is one of the most basic layout widgets in Flutter.
       // It takes any number of children and puts them in a column from top to bottom.
       // By default, the column visually places its children at the top.
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          // This second [Text] widget takes appState, and accesses the only member of that class, current (which is a [WordPair]).
-          // [WordPair] provides several helpful getters, such as asPascalCase or asSnakeCase. Here, we use asLowerCase
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: () {
-              // Calls the getNext method on the appState object.
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ], // Notice how Flutter code makes heavy use of trailing commas.
-        // This particular comma doesn't need to be here, because children is the last (and also only) member of this particular Column parameter list.
-        // Yet it is generally a good idea to use trailing commas: they make adding more members trivial, and they also serve as a hint for Dart's
-        // auto-formatter to put a newline there.
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // This [Text] widget takes appState, and accesses the only member of that class, current (which is a [WordPair]).
+            // [WordPair] provides several helpful getters, such as asPascalCase or asSnakeCase. Here, we use asLowerCase
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // Calls the toggleFavorite method on the appState object.
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(
+                    appState.favorites.contains(pair)
+                        ? Icons.favorite
+                        : Icons.favorite_border,
+                  ),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Calls the getNext method on the appState object.
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
+            ),
+          ], // Notice how Flutter code makes heavy use of trailing commas.
+          // This particular comma doesn't need to be here, because children is the last (and also only) member of this particular Column parameter list.
+          // Yet it is generally a good idea to use trailing commas: they make adding more members trivial, and they also serve as a hint for Dart's
+          // auto-formatter to put a newline there.
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    // By using theme.textTheme, you access the app's font theme.
+    // This class includes members such as bodyMedium (for standard text of medium size), caption (for captions of images), or headlineLarge (for large headlines).
+    // The displayMedium property is a large style meant for display text. The word display is used in the typographic sense here, such as in display typeface.
+    // The documentation for displayMedium says that "display styles are reserved for short, important text"—exactly our use case
+    // Calling copyWith() on displayMedium returns a copy of the text style with the changes you define. In this case, you're only changing the text's color.
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      // First, the code requests the app's current theme with Theme.of(context)
+      // Then, the code defines the card's color to be the same as the theme's colorScheme property.
+      // The color scheme contains many colors, and primary is the most prominent, defining color of the app.
+      color: theme.colorScheme.primary,
+      elevation: 8,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          // for accessibility.
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
